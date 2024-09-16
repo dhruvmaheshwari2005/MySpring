ff1b62a84bc8db173e745087791002efe916b91a
dhruvmaheshwari2005 <dhruvm2005@yahoo.com>
2024-09-16 11:29:34 +0530

    Initial commit

---
+++ MySpring/src/main/java/com/dhruv/MySpring/Example.java
@@ -0,0 +1,10 @@
+package com.dhruv.MySpring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Example {
+    public void  doNothing() {
+        System.out.println("Do nothing");
+    }
+}
---
+++ MySpring/src/main/java/com/dhruv/MySpring/MySpringApplication.java
@@ -0,0 +1,18 @@
+package com.dhruv.MySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MySpringApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context =
+				SpringApplication.run(MySpringApplication.class, args);
+
+		Example e = context.getBean(Example.class);
+		e.doNothing();
+	}
+
+}
